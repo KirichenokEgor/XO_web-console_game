2b1e9b6eafee51f2a7a980b857636be0e7b00a5c
Kirichenok Egor <yayoyu@list.ru>
2019-05-04 10:08:32 +0300

    Created Maven quickstart project

---
+++ src/main/java/com/KE-RA-D/App.java
@@ -0,0 +1,13 @@
+package com.KE-RA-D;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
